eb2f7a757b20b17fb594fab9d7f0f5bf4d346106
aryans <aryan.chablani@mail.utoronto.ca>
2022-09-14 23:15:00 -0400

    Completed all the TODO tasks for the Intro to Java tasks

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -40,9 +40,10 @@ public class Basics {
          */
         System.out.println(7 + 5);
 
-        /* TODO (Task 1): Write a line of code below that prints the string
+        /* TOD (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -59,9 +60,10 @@ public class Basics {
          */
 
         /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
+         * TOD (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -88,7 +90,7 @@ public class Basics {
          */
 
         /*
-         * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
+         * TOD (Task 3): Create a for-loop that loops from 10 to 0, printing
          *                the following at each iteration:
          *                Current count: #
          *
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -143,6 +148,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] wordList = to_split.split(" ");
+        for (String i : wordList){
+            ret.append(i.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -164,19 +173,23 @@ public class Basics {
     public static int oddSum(int[] arr) {
         int current_sum = 0;
 
-        /* TODO (Task 5): Complete this method body using a for-loop.
+        /* TOD (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        if (arr.length > 1) {
+            for (int i = 1; i < arr.length; i = i+2){
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
 
     /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
+     * TOD (Task 6): Submit the changes you made on GitHub!
      *                When you submit it, go to the 'Actions' tab. You should
      *                be able to see this exercise being autograded!
      *
